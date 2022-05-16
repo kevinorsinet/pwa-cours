450d672e694ef6ae29f6a08397c4b8d76c4539fe
kevinorsinet <kevin.orsinet@gmail.com>
2022-05-16 13:34:17 +0200

    correction bug

--- pages/_app.js
+++ pages/_app.js
@@ -2,44 +2,9 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return 
-  (<>
-    <Head>
-      <meta name='application-name' content='PWA App' />
-      <meta name='apple-mobile-web-app-capable' content='yes' />
-      <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-      <meta name='apple-mobile-web-app-title' content='PWA App' />
-      <meta name='description' content='Best PWA App in the world' />
-      <meta name='format-detection' content='telephone=no' />
-      <meta name='mobile-web-app-capable' content='yes' />
-      <meta name='msapplication-config' content='/browserconfig.xml' />
-      <meta name='msapplication-TileColor' content='#2B5797' />
-      <meta name='msapplication-tap-highlight' content='no' />
-      <meta name='theme-color' content='#000000' />
-
-      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-      <link rel='manifest' href='/manifest.json' />
-      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
-      <link rel='shortcut icon' href='/favicon.ico' />
-      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
-          
-      <meta name='twitter:card' content='summary' />
-      <meta name='twitter:url' content='https://yourdomain.com' />
-      <meta name='twitter:title' content='PWA App' />
-      <meta name='twitter:description' content='Best PWA App in the world' />
-      <meta name='twitter:image' content='https://yourdomain.com/android-chrome-192x192.png' />
-      <meta name='twitter:creator' content='@DavidWShadow' />
-      <meta property='og:type' content='website' />
-      <meta property='og:title' content='PWA App' />
-      <meta property='og:description' content='Best PWA App in the world' />
-      <meta property='og:site_name' content='PWA App' />
-      <meta property='og:url' content='https://yourdomain.com' />
-      <meta property='og:image' content='https://yourdomain.com/apple-touch-icon.png' />
-
-    </Head>
+  (
     <Component {...pageProps} />
-  
-  </>
+    
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -16,3 +16,5 @@ class MyDocument extends Document {
     )
   }
 }
+
+export default MyDocument;
